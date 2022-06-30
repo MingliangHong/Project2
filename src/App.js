cdc8952d57b5e2b55b6df2ef2446ed511811819f
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2022-06-30 08:19:11 -0400

    3.3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
-import { HashRouter, Route, Routes} from 'react-router-dom';
+import {  BrowserRouter, Route, Routes} from 'react-router-dom';
 import Detailpage from './Detailpage';
 import Navbar from './Navbar';
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Navbar></Navbar>
       <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path='/detailpage' element={<Detailpage></Detailpage> }></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
