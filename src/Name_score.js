af564126fde821fed8f3f35fc9599c22cac0a59c
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2022-06-30 08:30:44 -0400

    3.4

--- src/Name_score.js
+++ src/Name_score.js
@@ -2,7 +2,7 @@ import { FcLike } from "react-icons/fc";
 import { Link } from "react-router-dom";
 const Name_score = (props) => {
     return (
-        <Link className="name_with_score" tp="./detailpage">
+        <Link className="name_with_score" to ="./detailpage">
             <h3>{props.name}</h3>
             <h3>{props.score}<FcLike/></h3>
         </Link>
