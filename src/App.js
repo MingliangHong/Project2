f1a5a60c32680e3b2ae92df275267b2de0d9de07
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2022-06-30 07:20:26 -0400

    2.9

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 import Detailpage from './Detailpage';
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <Navbar/>
         <div className='content'>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/detailpage">
-              <Detailpage />
-            </Route>
-          </Switch>
+        <Route exact path="/" element={<Home></Home>}></Route>
+        <Route exact path='/detailpage' element={<detailpage></detailpage>}></Route>
         </div>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
