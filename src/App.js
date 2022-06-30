51386c3293fb8431236090028119b258b8b00626
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2022-06-30 06:26:57 -0400

    2.1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Navbar/>
+        <Home></Home>
         <div className='content'>
           <Switch>
             <Route exact path="/">
